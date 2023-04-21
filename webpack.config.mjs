9edd1b2d4ceec2216a55816722308188cb457204
Marc Horne <me@marcushorne.com>
2023-04-21 15:00:27 +0100

    feat: add webpack

---
+++ webpack.config.mjs
@@ -0,0 +1,47 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  target: 'node',
+  entry: './server/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
+  externals: [
+    function ({ request }, callback) {
+      if (request === 'express') {
+        return callback(null, 'commonjs ' + request);
+      }
+      callback();
+    },
+  ],
+};
