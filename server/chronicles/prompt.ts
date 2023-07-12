0c2ce4feee8af5173f85a3778b5191bc20297c1a
Marcus Horne <marc.horne@amorelie.com>
2023-07-12 14:26:35 +0100

    remove roll_dice

--- server/chronicles/prompt.ts
+++ server/chronicles/prompt.ts
@@ -1,10 +1,7 @@
 import { Request, Response } from 'express';
 import { ChatCompletionRequestMessage } from 'openai';
 
-import {
-	STORY_PROMPT_SYSTEM_MESSAGE,
-	ROLL_PROMPT_SYSTEM_MESSAGE,
-} from '@/consts/chronicles/prompts';
+import { STORY_PROMPT_SYSTEM_MESSAGE } from '@/consts/chronicles/prompts';
 import { openai } from '@/utilities/openai';
 
 export default async function promptHandler(req: Request, res: Response) {
@@ -34,30 +31,11 @@ export default async function promptHandler(req: Request, res: Response) {
 		const story: string | undefined =
 			storyCompletion.data.choices[0].message?.content;
 
-		const rollCompletionResponse = await rollCompletion(story as string);
-
 		res.status(200).json({
 			story,
-			roll_dice: rollCompletionResponse?.toLowerCase().includes('true'),
 		});
 	} catch (error: any) {
 		console.log(error.response.data);
 		res.status(500).json({ error: error.response.data });
 	}
 }
-
-const rollCompletion = async (story: string) => {
-	try {
-		const rollCompletion = await openai.createCompletion({
-			model: 'text-davinci-003',
-			prompt: await ROLL_PROMPT_SYSTEM_MESSAGE.format({ story }),
-			temperature: 0,
-			max_tokens: 200,
-		});
-
-		return rollCompletion.data.choices[0].text;
-	} catch (error: any) {
-		console.log(error.response.data);
-		return;
-	}
-};
