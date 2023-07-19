76cbc4eb2b1ab0f0bcc179ecabd1abf7bd062c10
Marcus Horne <marc.horne@amorelie.com>
2023-07-19 17:08:13 +0100

    Fix live chat crashing

--- server/chronicles/liveChat.ts
+++ server/chronicles/liveChat.ts
@@ -8,66 +8,58 @@ const youtube = google.youtube({
 
 export default async function youTubeChatHandler(req: Request, res: Response) {
 	try {
-		const messages = await getRecentChatMessages({ videoId: req.body.videoId });
-		res.status(200).json({ messages });
+		const videoId = req.body.videoId;
+
+		// Get Live Chat ID
+		const response = await youtube.videos.list({
+			id: [videoId],
+			part: ['liveStreamingDetails'],
+		});
+
+		if (
+			!response.data.items ||
+			!response.data.items[0] ||
+			!response.data.items[0].liveStreamingDetails
+		) {
+			throw new Error('Live chat ID not found');
+		}
+
+		const liveChatId =
+			response.data.items[0].liveStreamingDetails.activeLiveChatId || '';
+
+		// Get chat messages
+		const chatResponse = await youtube.liveChatMessages.list({
+			liveChatId,
+			part: ['snippet', 'authorDetails'],
+		});
+
+		const messages = chatResponse.data.items?.map((item: any) => {
+			const formattedMessage = item.snippet.displayMessage
+				.toLowerCase()
+				.replace(/\s/g, '');
+
+			const includesTaggedUser =
+				formattedMessage.includes('@48chronicles') ||
+				formattedMessage.includes('@48c');
+
+			return {
+				message: item.snippet.displayMessage,
+				username: item.authorDetails.displayName,
+				timestamp: item.snippet.publishedAt,
+				includesTaggedUser,
+			};
+		});
+
+		const currentTime = Date.now();
+
+		const recentChats = messages?.filter((message: any) => {
+			const messageTime = Date.parse(message.timestamp);
+			return currentTime - messageTime <= 60000;
+		});
+
+		res.status(200).json({ messages: recentChats });
 	} catch (error: any) {
 		console.log(error);
-		res.status(500).json({ error: error.response.data });
+		res.status(500).json({ error: error.message });
 	}
-}
-
-async function getRecentChatMessages({ videoId }: { videoId: string }) {
-	const liveChatId = await getLiveChatId({ videoId });
-
-	const messages = await getChatMessages(liveChatId as string);
-
-	const currentTime = Date.now();
-
-	const recentChats = messages?.filter((message: any) => {
-		const messageTime = Date.parse(message.timestamp);
-		return currentTime - messageTime <= 60000;
-	});
-
-	return recentChats;
-}
-
-async function getLiveChatId({ videoId }: { videoId: string }) {
-	const response = await youtube.videos.list({
-		id: [videoId],
-		part: ['liveStreamingDetails'],
-	});
-
-	if (
-		response.data.items &&
-		response.data.items[0] &&
-		response.data.items[0].liveStreamingDetails
-	) {
-		return response.data.items[0].liveStreamingDetails.activeLiveChatId;
-	} else {
-		throw new Error('Live chat ID not found');
-	}
-}
-
-async function getChatMessages(liveChatId: string) {
-	const response = await youtube.liveChatMessages.list({
-		liveChatId: liveChatId,
-		part: ['snippet', 'authorDetails'],
-	});
-
-	return response.data.items?.map((item: any) => {
-		const formattedMessage = item.snippet.displayMessage
-			.toLowerCase()
-			.replace(/\s/g, '');
-
-		const includesTaggedUser =
-			formattedMessage.includes('@48chronicles') ||
-			formattedMessage.includes('@48c');
-
-		return {
-			message: item.snippet.displayMessage,
-			username: item.authorDetails.displayName,
-			timestamp: item.snippet.publishedAt,
-			includesTaggedUser,
-		};
-	});
 }
