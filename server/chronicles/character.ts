716d10c5e34c6edb628647fbe27ce569feb10e80
Marcus Horne <marc.horne@amorelie.com>
2024-01-21 20:53:18 +0000

    update model

--- server/chronicles/character.ts
+++ server/chronicles/character.ts
@@ -5,7 +5,7 @@ import { GENERATE_CHARACTER } from '@/consts/chronicles/prompts';
 
 export default async function characterHandler(req: Request, res: Response) {
 	const completion = await openai.createCompletion({
-		model: 'text-davinci-003',
+		model: 'gpt-3.5-turbo-instruct',
 		prompt: GENERATE_CHARACTER,
 		max_tokens: 2000,
 		temperature: 0.8,
@@ -14,7 +14,7 @@ export default async function characterHandler(req: Request, res: Response) {
 	const character = completion.data.choices[0].text;
 
 	const completion2 = await openai.createCompletion({
-		model: 'text-davinci-003',
+		model: 'gpt-3.5-turbo-instruct',
 		prompt: `Transform this JSON object into a valid JSON object with no errors: ${character}`,
 		max_tokens: 2000,
 		temperature: 0,
