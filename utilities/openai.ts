d73d1826ffe40e80bef3fdfd7319712b0309dc8b
Marcus Horne <marc.horne@amorelie.com>
2024-01-21 20:40:38 +0000

    add org

--- utilities/openai.ts
+++ utilities/openai.ts
@@ -4,7 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+	apiKey: process.env.OPENAI_API_KEY,
+	organization: process.env.OPENAI_API_ORGANISATION,
 });
 
 export const openai = new OpenAIApi(configuration);
